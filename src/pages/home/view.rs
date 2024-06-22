41a251c8a3226817b0aabd49413ecb9a23480ec8
Akin Aguda <lordtitan7@gmail.com>
2024-06-22 11:49:50 +0100

    tailwind and sass work now

--- src/pages/home/view.rs
+++ src/pages/home/view.rs
@@ -4,11 +4,12 @@ use leptos::*;
 #[component]
 pub fn HomePage() -> impl IntoView {
     // Creates a reactive value to update the button
-    let (count, set_count) = create_signal(0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
+    // let (count, set_count) = create_signal(0);
+    // let on_click = move |_| set_count.update(|count| *count += 1);
 
     view! {
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
+      <section class="bg-red-300 py-10">
+        Hello
+      </section>
     }
 }
