7a1f64aaf90c5fcb040cb69132013a5221fa5cc6
benwis <ben@celcyon.com>
2024-09-25 20:35:39 -0700

    Added Dockerfile, fixed errors from component, and setup GH Action for Deployment to Fly.io

--- src/components.rs
+++
@@ -1,2 +0,0 @@
-pub mod community;
-pub use crate::components::community;
--- src/pages/home/view.rs
+++ src/pages/home/view.rs
@@ -1,7 +1,7 @@
 use leptos::*;
 
 /// Renders the home page of your application.
-use crate::components::community;
+use crate::components::community::Community;
 
 #[component]
 pub fn HomePage() -> impl IntoView {
@@ -10,13 +10,13 @@ pub fn HomePage() -> impl IntoView {
     // let on_click = move |_| set_count.update(|count| *count += 1);
 
     view! {
-      <section class="pt-10 h-screen bg-background-main min-h-fit">
-        <nav class="max-w-6xl w-full">
-        <ul>
-          <li>Hello World</li>
-          <Community/>
-        </ul>
-        </nav>
-      </section>
+        <section class="pt-10 h-screen bg-background-main min-h-fit">
+            <nav class="max-w-6xl w-full">
+                <ul>
+                    <li>Hello World</li>
+                    <Community/>
+                </ul>
+            </nav>
+        </section>
     }
 }
