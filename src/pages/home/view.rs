741f0e5f3431a927cc781da8300a9166832864f3
Akin Aguda <lordtitan7@gmail.com>
2024-10-15 12:41:34 -0500

    commit before bug report

--- src/pages/home/view.rs
+++ src/pages/home/view.rs
@@ -1,14 +1,7 @@
 use leptos::prelude::*;
 
-/// Renders the home page of your application.
-use crate::components::community::Community;
-
 #[component]
 pub fn HomePage() -> impl IntoView {
-    // Creates a reactive value to update the button
-    // let (count, set_count) = create_signal(0);
-    // let on_click = move |_| set_count.update(|count| *count += 1);
-
     view! {
         <section class="pt-10 bg-background-main h-[200vh] min-h-fit">
 
