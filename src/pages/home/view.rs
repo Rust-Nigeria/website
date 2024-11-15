9a3c6df797dd3a8a01168e5b8ce5ad8bfedc3f9e
Akin Aguda <lordtitan7@gmail.com>
2024-11-15 13:35:14 -0600

    typography fix

--- src/pages/home/view.rs
+++ src/pages/home/view.rs
@@ -3,8 +3,9 @@ use leptos::prelude::*;
 #[component]
 pub fn HomePage() -> impl IntoView {
     view! {
-        <section class="pt-10 bg-background-main h-[200vh] min-h-fit">
-
-        </section>
-    }
+            <section class="pt-40 bg-background-main h-[200vh] min-h-fit">
+    <h1 class="max-w-[897px]">Your #1 Community for Rust Enthusiasts</h1>
+    <h2 class="max-w-[897px]">Rust Events</h2>
+            </section>
+        }
 }
