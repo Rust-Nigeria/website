57e0f23357f363b00fe766d184f7281af9767570
Akin Aguda <lordtitan7@gmail.com>
2024-11-15 11:57:36 -0600

    some work on community section

---
+++ src/pages/home/community_section/view.rs
@@ -0,0 +1,13 @@
+use leptos::prelude::*;
+
+#[component]
+pub fn CommunitySection() -> impl IntoView {
+    view! {
+        <section class="pt-10 bg-background-dark mt-10 w-full flex flex-col items-center">
+            <div class="max-w-60 w-full text-grey-60">
+                <h6>Over</h6>
+                <h1>400</h1>
+            </div>
+        </section>
+    }
+}
--- src/pages/home/view.rs
+++ src/pages/home/view.rs
@@ -1,10 +1,12 @@
+use crate::pages::home::community_section::view::CommunitySection;
 use leptos::prelude::*;
 
 #[component]
 pub fn HomePage() -> impl IntoView {
     view! {
-        <section class="pt-10 bg-background-main h-[200vh] min-h-fit">
-
-        </section>
+        <div class="w-full">
+            <section class="h-screen"></section> // This is a placeholder for the homepage her
+             <CommunitySection />
+        </div>
     }
 }
