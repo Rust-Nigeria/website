12edcf0e16e65822d23007f48b3e5bc53ada4317
Akin Aguda <lordtitan7@gmail.com>
2024-11-15 15:34:27 -0600

    Resolved conflicts

--- src/pages/home/community_section/view.rs
+++ src/pages/home/community_section/view.rs
@@ -5,8 +5,8 @@ pub fn CommunitySection() -> impl IntoView {
     view! {
         <section class="pt-10 bg-background-dark mt-10 w-full flex flex-col items-center">
             <div class="max-w-60 w-full text-grey-60">
-                <h6>Over</h6>
-                <h1>400</h1>
+                <p class="text-2xl">Over</p>
+                <p class="text-8xl text-grey-90 font-bold">400</p>
             </div>
         </section>
     }
