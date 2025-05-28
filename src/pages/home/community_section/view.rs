956b927c777b2feae0da9b7df9c39d6067845dd6
Akin Aguda <lordtitan7@gmail.com>
2025-05-28 11:53:20 -0400

    before version upgrade

--- src/pages/home/community_section/view.rs
+++ src/pages/home/community_section/view.rs
@@ -1,13 +1,21 @@
+use crate::{
+    cn,
+    components::button::view::{Button, ButtonColorVariants, ButtonSizeVariants, ButtonUsecase},
+};
 use leptos::prelude::*;
 
-use crate::components::button::view::{
-    Button, ButtonColorVariants, ButtonSizeVariants, ButtonUsecase,
-};
-
-use super::members_jigsaw::view::MembersJigsaw;
+use leptos::html::Img;
+use leptos_use::use_intersection_observer;
 
 #[component]
 pub fn CommunitySection() -> impl IntoView {
+    let image_el = NodeRef::<Img>::new();
+    let (is_image_visible, set_is_image_visible) = signal(false);
+
+    use_intersection_observer(image_el, move |entries, _| {
+        set_is_image_visible(entries[0].is_intersecting());
+    });
+
     view! {
         <section class="pt-10 px-6 bg-background-dark py-10 w-full flex flex-col items-center">
             <div class="w-full text-grey-60">
@@ -26,8 +34,10 @@ pub fn CommunitySection() -> impl IntoView {
             </div>
 
             <div class="w-full mt-8 max-w-[858px]">
-                <MembersJigsaw />
-                // <img src="/assets/images/puzzle.png" class="aspect-[857/571] w-full" />
+                <img node_ref={image_el} src="/assets/images/puzzle.png" class=cn!(#(
+                    "aspect-[857/571] w-full opacity-0 translate-y-20",
+                    (is_image_visible(), "opacity-100 translate-y-0 transition-all duration-500")
+                )) />
             </div>
         </section>
     }
