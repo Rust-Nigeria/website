3ed99a10b2a1f60f8d80942ef99ab8759365ce7d
Akin Aguda <lordtitan7@gmail.com>
2025-06-12 13:14:49 -0500

    Now animating all the components. Need to tweak interception observer to receiver params

--- src/components/reveal_text_line/view.rs
+++ src/components/reveal_text_line/view.rs
@@ -1,21 +1,24 @@
-use leptos::{ev, prelude::*};
+use leptos::prelude::*;
 
 use crate::cn;
 
 #[component]
 pub fn RevealTextLine(
     children: Children,
-    reveal: bool,
+    reveal: ReadSignal<bool>,
     #[prop(default = "")] class: &'static str,
     #[prop(default = "")] parent_class: &'static str,
     #[prop(default = false)] rotate: bool,
     #[prop(default = 0)] delay: u16,
 ) -> impl IntoView {
+    let delay_style = format!("transition-delay: {}ms", delay);
     view! {
         <span class=cn!(#("overflow-y-hidden inline-flex", parent_class))>
-            <span class=cn!(#(
+            <span
+            style=delay_style
+             class=cn!(#(
                 cn!("translate-y-full inline-flex opacity-0 duration-500", (rotate, "rotate-6 origin-top-left")),
-                (reveal, "translate-y-0 opacity-100", (rotate, "rotate-0")),
+                (reveal(), "translate-y-0 opacity-100", (rotate, "rotate-0")),
                 class
             ))>
                {children()}
--- src/pages/home/community_section/view.rs
+++ src/pages/home/community_section/view.rs
@@ -1,6 +1,9 @@
 use crate::{
     cn,
-    components::button::view::{Button, ButtonColorVariants, ButtonSizeVariants, ButtonUsecase},
+    components::{
+        button::view::{Button, ButtonColorVariants, ButtonSizeVariants, ButtonUsecase},
+        reveal_text_line::view::RevealTextLine,
+    },
     hooks::use_in_view::{use_in_view, ElementVisibilityData},
 };
 use leptos::html::Img;
@@ -15,18 +18,36 @@ pub fn CommunitySection() -> impl IntoView {
     view! {
         <section class="pt-10 px-6 bg-background-dark py-10 w-full flex flex-col items-center">
             <div class="w-full text-grey-60">
-                <p class="text-2xl lg:text-3xl">Over</p>
-                <p class="text-7xl lg:text-8xl mt-3 text-grey-90 font-bold">500</p>
-                <p class="mt-3 text-lg sm:text-2xl lg:text-3xl">Community Members</p>
-                <p class="mt-3 text-base sm:text-xl lg:text-2xl">You are the missing piece</p>
-                <Button
-                    class="animate-scale-in mt-6 px-24"
-                    use_as=ButtonUsecase::Link { href: String::from("https://rustnigeria.curated.co/") }
-                    color=ButtonColorVariants::White
-                    size=ButtonSizeVariants::Lg
-                >
-                    Join Us!
-                </Button>
+                <p class="text-2xl lg:text-3xl">
+                    <RevealTextLine reveal={in_view}>
+                        Over
+                    </RevealTextLine>
+                </p>
+                <p class="text-7xl lg:text-8xl mt-3 text-grey-90 font-bold">
+                    <RevealTextLine delay={100} reveal={in_view}>
+                        500
+                    </RevealTextLine>
+                </p>
+                <p class="mt-3 text-lg sm:text-2xl lg:text-3xl">
+                    <RevealTextLine delay={200} reveal={in_view}>
+                        Community Members
+                    </RevealTextLine>
+                </p>
+                <p class="mt-3 text-base sm:text-xl lg:text-2xl">
+                    <RevealTextLine delay={300} reveal={in_view}>
+                       You are the missing piece
+                    </RevealTextLine>
+                </p>
+                <RevealTextLine delay={400} reveal={in_view}>
+                    <Button
+                        class="animate-scale-in mt-6 px-24"
+                        use_as=ButtonUsecase::Link { href: String::from("https://rustnigeria.curated.co/") }
+                        color=ButtonColorVariants::White
+                        size=ButtonSizeVariants::Lg
+                    >
+                        Join Us!
+                    </Button>
+                </RevealTextLine>
             </div>
 
             <div class="w-full mt-8 max-w-[858px]">
