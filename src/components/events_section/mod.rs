48b213db1bf29a9515c74523958b54131f247e75
Akin Aguda <lordtitan7@gmail.com>
2025-06-23 06:35:32 -0500

    come work con community section

---
+++ src/components/events_section/mod.rs
@@ -0,0 +1,8 @@
+use leptos::prelude::*;
+
+#[component]
+pub fn EventsSection() -> impl IntoView {
+    view! {
+        <div class="text-white bg-grey-10 flex justify-center px-6">Hello</div>
+    }
+}
