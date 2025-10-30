c97745fca080b301a79eca5ed89f24c96fdb2df6
Akin Aguda <lordtitan7@gmail.com>
2025-10-30 15:20:01 -0500

    added community stats

--- src/components/community_stats/mod.rs
+++ src/components/community_stats/mod.rs
@@ -2,30 +2,54 @@ mod stats;
 
 use leptos::prelude::*;
 
-use crate::components::button::{Button, ButtonColorVariants, ButtonSizeVariants, ButtonUsecase};
-use crate::constants::urls;
-
+use leptos::html::Div;
 use stats::Stats;
+
+use crate::constants::urls;
+use crate::{
+    cn,
+    components::{
+        button::{Button, ButtonColorVariants, ButtonSizeVariants, ButtonUsecase},
+        reveal_text_line::RevealTextLine,
+    },
+    hooks::use_in_view::{use_in_view, ElementVisibilityData},
+};
 
 #[component]
 pub fn CommunityStats() -> impl IntoView {
+    let section_ref = NodeRef::<Div>::new();
+    let ElementVisibilityData {
+        in_view: section_in_view,
+    } = use_in_view(section_ref, None);
     view! {
         <div class="bg-background-dark text-grey-100 px-6 py-10 gap-y-8 flex flex-col w-full items-center">
-           <div class="w-full max-w-7xl flex flex-col items-start">
-                <h2 class="header-2 text-left max-w-4xl">
-                    We have fallen in love with Rust for many reasons
+           <div node_ref=section_ref class="w-full max-w-7xl flex flex-col items-center">
+                <h2 class=cn!(#(
+                    "header-2 text-center max-w-4xl opacity-0 translate-y-8 duration-500",
+                     (section_in_view(), "opacity-100 translate-y-0")
+                ))>
+                    <RevealTextLine reveal={section_in_view}>
+                         We have fallen in love with Rust for many reasons
+                    </RevealTextLine>
                 </h2>
-                <p class="mt-3 text-left header-6 text-grey-70 max-w-3xl">
+                <p class=cn!(#(
+                    "mt-3 text-center header-6 text-grey-70 max-w-3xl opacity-0 translate-y-8 duration-500",
+                     (section_in_view(), "opacity-100 translate-y-0 delay-200")
+                ))>
                     Numerous surveys, awards won, the resilient community backing the language are among the reasons we have fallen in love with Rust
                 </p>
-                 <Button
-                    class="animate-scale-in mt-6"
-                    use_as=ButtonUsecase::Link { href: String::from(urls::JOIN_US) }
-                    color=ButtonColorVariants::White
-                    size=ButtonSizeVariants::Lg
-                >
-                    Join the Community
-                </Button>
+                <div class=cn!(#(
+                    "opacity-0 translate-y-8 duration-500 mt-6",
+                     (section_in_view(), "opacity-100 translate-y-0 delay-300")
+                ))>
+                    <Button
+                        use_as=ButtonUsecase::Link { href: String::from(urls::JOIN_US) }
+                        color=ButtonColorVariants::White
+                        size=ButtonSizeVariants::Lg
+                    >
+                        Join the Community
+                    </Button>
+                </div>
            </div>
            <Stats />
         </div>
--- src/components/community_stats/stats/mod.rs
+++ src/components/community_stats/stats/mod.rs
@@ -7,7 +7,7 @@ use leptos::prelude::*;
 use stylance::*;
 
 use crate::{
-    components::rocket::Rocket,
+    components::{reveal_text_line::RevealTextLine, rocket::Rocket},
     hooks::use_in_view::{use_in_view, ElementVisibilityData},
 };
 
@@ -22,10 +22,24 @@ pub fn Stats() -> impl IntoView {
     } = use_in_view(section_ref, None);
 
     view! {
-        <div class="w-full max-w-7xl flex h-[700px] items-end justify-center bg-grey-20 rounded-3xl overflow-hidden">
+        <div class="w-full max-w-7xl flex h-[500px] sm:h-[700px] items-end justify-center bg-grey-20 rounded-3xl overflow-hidden">
            <div node_ref=section_ref class="w-full h-full relative flex items-center justify-center">
+            <div class="absolute text-white left-0 top-0 w-full h-full pl-6 pt-10 sm:pl-20 sm:pt-20">
+                <div class="text-left w-fit">
+                    <p class="huge-label">
+                        <RevealTextLine rotate={true} reveal={section_in_view}>
+                            Over 700
+                        </RevealTextLine>
+                    </p>
+                    <p class="header-2 mt-3">
+                        <RevealTextLine reveal={section_in_view} delay={100}>
+                            Community Members and growing!
+                        </RevealTextLine>
+                    </p>
+                </div>
+             </div>
 
-             <div>
+             <div class=classes::rocketContainer>
                 <Rocket
                     class=cn!(#(
                         classes::rocket,
@@ -36,10 +50,14 @@ pub fn Stats() -> impl IntoView {
                     ))
                 />
              </div>
-             <div class="absolute w-full bottom-0 left-0">
-                <SmokeLayer1 {..} class="absolute w-full bottom-0 left-0" />
-                <SmokeLayer2 {..} class="absolute w-full bottom-0 left-0" />
-                <SmokeLayer3 {..} class="absolute w-full bottom-0 left-0" />
+
+             <div class=cn!(#(
+                "absolute h-full w-full bottom-0 overflow-hidden", classes::smoke,
+                (section_in_view(), classes::animateSmoke)
+             ))>
+                <SmokeLayer1 {..} class="absolute w-full bottom-0" />
+                <SmokeLayer2 {..} class="absolute w-full bottom-0" />
+                <SmokeLayer3 {..} class="absolute w-full bottom-0" />
              </div>
            </div>
         </div>
