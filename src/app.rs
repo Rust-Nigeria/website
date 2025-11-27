0a31c1b3c6d1f16ffd7995f14c4a44cd03bfcd6b
Akin Aguda <lordtitan7@gmail.com>
2025-11-27 10:25:13 -0600

    updated meta

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,5 @@
 use leptos::prelude::*;
-use leptos_meta::{provide_meta_context, Stylesheet, Title};
+use leptos_meta::{provide_meta_context, Meta, Stylesheet, Title};
 use leptos_router::{
     components::{Route, Router, Routes},
     StaticSegment, WildcardSegment,
@@ -19,7 +19,7 @@ pub fn App() -> impl IntoView {
         // injects a stylesheet into the document <head>
         // id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href="/pkg/rust-nigeria-website.css"/>
-
+        <Meta name="description" content="The Nigerian Rust community website. Find out about community projects, events and articles." />
         // sets the document title
         <Title text="Welcome to Rust Nigeria"/>
 
